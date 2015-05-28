ed68158477c1b6a8581adc49d324c5c2dbef698d
Chester Twomey <chester.twomey@gmail.com>
2015-05-27 20:21:47 -0500

    fix birdcage test, now uses temp dir and will work cross platform

--- processor/birdCage_test.go
+++ processor/birdCage_test.go
@@ -1,6 +1,7 @@
 package processor
 
 import (
+	"log"
 	"os"
 	"testing"
 
@@ -10,12 +11,15 @@ import (
 var birdsToCage []anaconda.Tweet = []anaconda.Tweet{anaconda.Tweet{Text: "today i'm @ bills sandwhiches corner of main and 1st"}, anaconda.Tweet{Text: "today i ate some toast and cheese, it wasn't very good"}, anaconda.Tweet{Text: "I smashed my elbow on the corner of a counter, ouchie"}, anaconda.Tweet{Text: "hanging out at the corner of 1st and main"}}
 
 func TestPuttingInCage(t *testing.T) {
-	birdcage := JSONFileCage{Filename: "tmp/test.json"}
+
+	filename := os.TempDir() + "/test.json"
+	log.Println("filename" + filename)
+	birdcage := JSONFileCage{Filename: filename}
 	birdcage.putInCage(&birdsToCage)
-	if _, err := os.Stat("/tmp/test.json"); os.IsNotExist(err) {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		t.Error("file doesn't exist")
 
 	}
-	os.Remove("tmp/test.json")
+	os.Remove(filename)
 
 }
